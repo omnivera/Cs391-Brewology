42f0c429ceb996fce3dc3bc3b58fee20e0750f3f
omnivera <mert.dallar@ozu.edu.tr>
2022-05-15 12:26:57 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<Navbar  className="brewnavbar">
+    <Container>
+      <Navbar.Brand style={{ color: "white" }} href="#home">
+       
+      Brewology 
+      </Navbar.Brand>
+    </Container>
+  </Navbar>
+<br></br>
+
+     <Row className="mx-0">
+  <Button as={Col} variant="primary">Button #1</Button>
+  <Button as={Col} variant="secondary" className="mx-2">Button #2</Button>
+  <Button as={Col} variant="success">Button #3</Button>
+</Row>
     </div>
   );
 }
