ecebb4041c982e08dfaca4b0d508da101aa7cb92
Esrah Zahid <israh.zahid@ozu.edu.tr>
2022-05-15 21:25:51 +0300

    Made changes to App.js + App.css

--- src/App.js
+++ src/App.js
@@ -12,33 +12,53 @@ function App() {
   return (
     <div className="App">
 
-      {/* <Navbar className="brewnavbar">
+      <Navbar className="nav-color">
         <Container>
-          <Navbar.Brand style={{ color: "white" }} href="#home">
-
-            Brewology
-          </Navbar.Brand>
+          <img
+            src = {require("./images/coffee-logo.png")}
+            width = "60"
+            height = "60"
+            className="d-inline-block align-top"
+            alt="coffee logo"
+          />
+          <Navbar.Brand href="#home" style={{ color: '#f0ebe0' }}>Brewology</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home" style={{ color: '#f0ebe0' }}>Home</Nav.Link>
+            <Nav.Link href="#features" style={{ color: '#f0ebe0' }}>Menu</Nav.Link>
+            <Nav.Link href="#pricing" style={{ color: '#f0ebe0' }}>Checkout</Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
-      <br></br>
+      <div className="Background"><p className="home-text">Some sample text about <br></br> how amazing our coffee is <br></br> goes here</p></div>
 
-      <Row className="mx-0">
-        <Button as={Col} variant="primary">Button #1</Button>
-        <Button as={Col} variant="secondary" className="mx-2">Button #2</Button>
-        <Button as={Col} variant="success">Button #3</Button>
-      </Row> */}
 
-      <Nav defaultActiveKey="/home" as="ul" className='brewnavbar'>
-        <Nav.Item as="li">
-          <Nav.Link href="/home"  style={{ color: "white" }} className='navLinks'>Active</Nav.Link>
-        </Nav.Item>
-        <Nav.Item as="li">
-          <Nav.Link eventKey="link-1" className='navLinks'>Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item as="li">
-          <Nav.Link eventKey="link-2" className='navLinks'>Link</Nav.Link>
-        </Nav.Item>
-      </Nav>
+
+      {/* <Navbar className="brewnavbar">
+      <Container>
+        <Navbar.Brand style={{ color: "white" }} href="#home">
+          Brewology
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+    <br></br>
+
+     <Row className="mx-0">
+      <Button as={Col} variant="primary">Button #1</Button>
+      <Button as={Col} variant="secondary" className="mx-2">Button #2</Button>
+      <Button as={Col} variant="success">Button #3</Button>
+    </Row>
+
+    <Nav defaultActiveKey="/home" as="ul" className='brewnavbar'>
+      <Nav.Item as="li">
+        <Nav.Link href="/home" style={{ color: "white" }} className='navLinks'>Active</Nav.Link>
+      </Nav.Item>
+      <Nav.Item as="li">
+        <Nav.Link eventKey="link-1" className='navLinks'>Link</Nav.Link>
+      </Nav.Item>
+      <Nav.Item as="li">
+        <Nav.Link eventKey="link-2" className='navLinks'>Link</Nav.Link>
+      </Nav.Item>
+    </Nav> */}
     </div>
   );
 }
